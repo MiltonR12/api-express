3bf750058e7c6821b7fa1bfc1618bcca064ea9c1
MiltonR12 <109563498+MiltonR12@users.noreply.github.com>
2022-11-05 12:33:41 -0400

    first commit

---
+++ src/controllers/index.controllers.js
@@ -0,0 +1,7 @@
+import { pool } from "../db.js";
+
+export const ping = async (req, res) => {
+  const [result] = await pool.query('SELECT 1 + 1 AS result')
+  console.log('entro')
+  res.json( result[0] )
+}
